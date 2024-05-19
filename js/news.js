ec762a91e12b2e14372fcd0e5089d81dc512da97
unknown <matheusghygino@gmail.com>
2024-05-19 19:57:51 -0300

    newsletter

---
+++ js/news.js
@@ -0,0 +1,26 @@
+const newsForm = document.getElementById('news-form');
+const newsInput = document.getElementById('news-input');
+
+export const enviarNews = newsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    newsInput.value = '';
+    mudarModal();
+    window.onclick = function(e) {
+        const modal = document.querySelector('.modal');
+        if (e.target == modal) {
+            mudarModal();
+        }
+    }
+});
+
+export const mudarModal = () => {
+    const modal = document.querySelector('.modal');
+    const estiloAtual = modal.style.display;
+    if(estiloAtual == 'block') {
+        modal.style.display = 'none';
+    }
+    else {
+        modal.style.display = 'block';
+    }
+}
+
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import { nextSlider } from "./banner.js";
 import { prevSlider } from "./banner.js";
+import { enviarNews } from "./news.js";
 
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
